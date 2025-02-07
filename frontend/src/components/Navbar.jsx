94067eda78b2f57d839f884ed46a3d86cc9c55f9
nischal puri <mrnischalpuri@gmail.com>
2025-02-07 18:47:21 +0545

    Hero Section completed

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import { NavLink, useNavigate } from "react-router-dom"
 const Navbar = () => {
   const navigate = useNavigate()
   const [showMenu, setShowMenu] = useState(false)
-  const [token, setToken] = useState(true)
+  const [token, setToken] = useState(false)
   // state for showing menu and token
   return (
     <div className="flex items-center justify-between text-sm py-4 mb-5 border-b border-b-gray-400">
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,12 @@
 import React from "react"
+import Header from "../components/Header"
 
 const Home = () => {
-  return <div className=""> Home</div>
+  return (
+    <div className="">
+      <Header />
+    </div>
+  )
 }
 
 export default Home
