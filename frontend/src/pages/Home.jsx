a0871091688c0761604045d0a727157a266c6cd5
nischal puri <mrnischalpuri@gmail.com>
2025-02-07 19:27:10 +0545

    doctor section added

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react"
 import Header from "../components/Header"
 import SepecialityMenu from "../components/SepecialityMenu"
+import TopDctors from "../components/TopDctors"
 
 const Home = () => {
   return (
     <div className="">
       <Header />
       <SepecialityMenu />
+      <TopDctors />
     </div>
   )
 }
